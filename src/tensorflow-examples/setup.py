7124bd4fb4f80050fa96cdc2ed61e5b1afc1747c
wilsonify <tom.andrew.wilson@gmail.com>
2019-11-16 09:46:38 -0500

    add code to project

---
+++ src/tensorflow-examples/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tensorflow_examples',
+    version='',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
