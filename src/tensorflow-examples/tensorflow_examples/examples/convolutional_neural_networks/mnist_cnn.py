cbc954f9342225409ec9437f7f77a89d7171a825
wilsonify <tom.andrew.wilson@gmail.com>
2019-11-17 10:40:28 -0500

    remove unused constants comment out dropout layers

--- src/tensorflow-examples/tensorflow_examples/examples/convolutional_neural_networks/mnist_cnn.py
+++ src/tensorflow-examples/tensorflow_examples/examples/convolutional_neural_networks/mnist_cnn.py
@@ -8,15 +8,12 @@ import logging
 import numpy as np
 import tensorflow as tf
 
-NUM_STEPS = 10
-MINIBATCH_SIZE = 50
-STEPS = 5000
 BATCH_SIZE = 128
 SHUFFLE_BUFFER_SIZE = 100
 IMAGE_SHAPE = (28, 28)
 INPUT_SHAPE = (28, 28, 1)
 NUM_CLASSES = 10
-EPOCHS = 12
+EPOCHS = 5
 
 
 def load_data():
@@ -62,10 +59,10 @@ def main():
                                      input_shape=INPUT_SHAPE))
     model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
     model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-    model.add(tf.keras.layers.Dropout(0.25))
+    # model.add(tf.keras.layers.Dropout(0.25))
     model.add(tf.keras.layers.Flatten())
     model.add(tf.keras.layers.Dense(128, activation='relu'))
-    model.add(tf.keras.layers.Dropout(0.5))
+    # model.add(tf.keras.layers.Dropout(0.5))
     model.add(tf.keras.layers.Dense(NUM_CLASSES, activation='softmax'))
 
     model.compile(loss=tf.keras.losses.categorical_crossentropy,
@@ -74,9 +71,9 @@ def main():
 
     model.fit(train_dataset,
               epochs=EPOCHS,
-              verbose=1,
+              verbose=2,
               )
-    score = model.evaluate(test_dataset, verbose=1)
+    score = model.evaluate(test_dataset, verbose=2)
     print('Test loss:', score[0])
     print('Test accuracy:', score[1])
 
