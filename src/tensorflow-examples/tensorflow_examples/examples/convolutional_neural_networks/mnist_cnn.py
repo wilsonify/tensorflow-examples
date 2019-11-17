84dd7cc9bd118e170c1b91001a317a0bcfb02834
wilsonify <tom.andrew.wilson@gmail.com>
2019-11-17 10:30:31 -0500

    resolve linting warnings

--- src/tensorflow-examples/tensorflow_examples/examples/convolutional_neural_networks/mnist_cnn.py
+++ src/tensorflow-examples/tensorflow_examples/examples/convolutional_neural_networks/mnist_cnn.py
@@ -1,59 +1,84 @@
+"""
+follow mnist example
+similar to softmax example, but with convolution added
+"""
+
+import logging
+
 import numpy as np
 import tensorflow as tf
-from keras.datasets.mnist import load_data
-from tensorflow_examples.layers import conv_layer, max_pool_2x2, full_layer
 
-DATA_DIR = '/tmp/data'
+NUM_STEPS = 10
 MINIBATCH_SIZE = 50
 STEPS = 5000
+BATCH_SIZE = 128
+SHUFFLE_BUFFER_SIZE = 100
+IMAGE_SHAPE = (28, 28)
+INPUT_SHAPE = (28, 28, 1)
+NUM_CLASSES = 10
+EPOCHS = 12
+
+
+def load_data():
+    """
+    get and split data
+    :return: tf.data.Dataset suitable for keras flows
+    """
+    data_url = "https://storage.googleapis.com/tensorflow/tf-keras-datasets/mnist.npz"
+    data_dir = "/tmp/data"
+    path = tf.keras.utils.get_file(
+        fname="mnist.npz", origin=data_url, cache_dir=data_dir
+    )
+
+    with np.load(path) as data:
+        train_examples = data["x_train"]
+        train_labels = tf.keras.utils.to_categorical(data["y_train"], NUM_CLASSES)
+        test_examples = data["x_test"]
+        test_labels = tf.keras.utils.to_categorical(data["y_test"], NUM_CLASSES)
+
+    train_dataset = tf.data.Dataset.from_tensor_slices((train_examples, train_labels))
+    test_dataset = tf.data.Dataset.from_tensor_slices((test_examples, test_labels))
+    logging.debug("%r", "type(train_dataset) = {}".format(type(train_dataset)))
+    logging.debug("%r", "type(test_dataset) = {}".format(type(test_dataset)))
+
+    train_dataset = train_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
+    test_dataset = test_dataset.batch(BATCH_SIZE)
+
+    return train_dataset, test_dataset
 
 
 def main():
-    mnist = load_data(path=DATA_DIR)
+    """
+    main function
+    load, train, evaluate
+    :return:
+    """
+    # input image dimensions
+    train_dataset, test_dataset = load_data()
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Conv2D(32, kernel_size=(3, 3),
+                                     activation='relu',
+                                     input_shape=INPUT_SHAPE))
+    model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(tf.keras.layers.Dropout(0.25))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(128, activation='relu'))
+    model.add(tf.keras.layers.Dropout(0.5))
+    model.add(tf.keras.layers.Dense(NUM_CLASSES, activation='softmax'))
 
-    x = tf.compat.v1.placeholder(tf.float32, shape=[None, 784])
-    y_ = tf.compat.v1.placeholder(tf.float32, shape=[None, 10])
+    model.compile(loss=tf.keras.losses.categorical_crossentropy,
+                  optimizer=tf.keras.optimizers.Adadelta(),
+                  metrics=['accuracy'])
 
-    x_image = tf.reshape(x, [-1, 28, 28, 1])
-    conv1 = conv_layer(x_image, shape=[5, 5, 1, 32])
-    conv1_pool = max_pool_2x2(conv1)
-
-    conv2 = conv_layer(conv1_pool, shape=[5, 5, 32, 64])
-    conv2_pool = max_pool_2x2(conv2)
-
-    conv2_flat = tf.reshape(conv2_pool, [-1, 7 * 7 * 64])
-    full_1 = tf.nn.relu(full_layer(conv2_flat, 1024))
-
-    keep_prob = tf.compat.v1.placeholder(tf.float32)
-    full1_drop = tf.nn.dropout(full_1, rate=1 - (keep_prob))
-
-    y_conv = full_layer(full1_drop, 10)
-
-    cross_entropy = tf.reduce_mean(
-        input_tensor=tf.nn.softmax_cross_entropy_with_logits(logits=y_conv, labels=tf.stop_gradient(y_)))
-    train_step = tf.compat.v1.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-    correct_prediction = tf.equal(tf.argmax(input=y_conv, axis=1), tf.argmax(input=y_, axis=1))
-    accuracy = tf.reduce_mean(input_tensor=tf.cast(correct_prediction, tf.float32))
-
-    with tf.compat.v1.Session() as sess:
-        sess.run(tf.compat.v1.global_variables_initializer())
-
-        for i in range(STEPS):
-            batch = mnist.train.next_batch(MINIBATCH_SIZE)
-
-            if i % 100 == 0:
-                train_accuracy = sess.run(accuracy, feed_dict={x: batch[0], y_: batch[1],
-                                                               keep_prob: 1.0})
-                print("step {}, training accuracy {}".format(i, train_accuracy))
-
-            sess.run(train_step, feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-
-        X = mnist.test.images.reshape(10, 1000, 784)
-        Y = mnist.test.labels.reshape(10, 1000, 10)
-        test_accuracy = np.mean(
-            [sess.run(accuracy, feed_dict={x: X[i], y_: Y[i], keep_prob: 1.0}) for i in range(10)])
-
-    print("test accuracy: {}".format(test_accuracy))
+    model.fit(train_dataset,
+              batch_size=BATCH_SIZE,
+              epochs=EPOCHS,
+              verbose=1,
+              )
+    score = model.evaluate(test_dataset, verbose=1)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
 
 
 if __name__ == "__main__":
