0ae5c5b2f39211b032208c71fc87fa0a91503c93
Tom Wilson <tom.andrew.wilson@gmail.com>
2019-11-17 16:26:05 -0500

    simple working example

--- src/tensorflow-examples/tensorflow_examples/examples/text_and_visualizations/vanilla_rnn_with_tfboard.py
+++ src/tensorflow-examples/tensorflow_examples/examples/text_and_visualizations/vanilla_rnn_with_tfboard.py
@@ -1,161 +1,74 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Dec 20 17:34:43 2016
-
-@author: tomhope
-"""
-from __future__ import print_function
-
 import tensorflow as tf
-# Import MINST data
-from keras.datasets.mnist import load_data
+from tensorflow.keras import Model, layers
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+
+num_classes = 10  # total classes (0-9 digits).
+num_features = 784  # data features (img shape: 28*28).
+learning_rate = 0.001
+training_steps = 1000
+batch_size = 32
+display_step = 100
+num_input = 28
+timesteps = 28
+num_units = 32
+
+
+class LSTM(Model):
+    def __init__(self):
+        super(LSTM, self).__init__()
+        self.lstm_layer = layers.LSTM(units=num_units)
+        self.out = layers.Dense(num_classes)
+
+    def call(self, x, is_training=False):
+        x = self.lstm_layer(x)
+        x = self.out(x)
+        if not is_training:
+            x = tf.nn.softmax(x)
+        return x
+
+
+def cross_entropy_loss(x, y):
+    y = tf.cast(y, tf.int64)
+    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=x)
+    return tf.reduce_mean(loss)
+
+
+def accuracy(y_pred, y_true):
+    correct_prediction = tf.equal(tf.argmax(y_pred, 1), tf.cast(y_true, tf.int64))
+    return tf.reduce_mean(tf.cast(correct_prediction, tf.float32), axis=-1)
+
+
+def run_optimization(x, y, lstm_net, optimizer):
+    with tf.GradientTape() as g:
+        pred = lstm_net(x, is_training=True)
+        loss = cross_entropy_loss(pred, y)
+    trainable_variables = lstm_net.trainable_variables
+    gradients = g.gradient(loss, trainable_variables)
+    optimizer.apply_gradients(zip(gradients, trainable_variables))
 
 
 def main():
-    tf.compat.v1.disable_eager_execution()
-    mnist = load_data(path="/tmp/data/")
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train, x_test = np.array(x_train, np.float32), np.array(x_test, np.float32)
+    x_train, x_test = x_train.reshape([-1, 28, 28]), x_test.reshape(
+        [-1, num_features])  # Flatten images to 1-D vector of 784 features (28*28).
+    x_train, x_test = x_train / 255., x_test / 255.  # Normalize images value from [0, 255] to [0, 1].
 
-    # Define some parameters
-    element_size = 28
-    time_steps = 28
-    num_classes = 10
-    batch_size = 128
-    hidden_layer_size = 128
+    train_data = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    train_data = train_data.repeat().shuffle(5000).batch(batch_size).prefetch(1)
 
-    # Where to save TensorBoard model summaries
-    LOG_DIR = "logs/RNN_with_summaries"
+    lstm_net = LSTM()
+    optimizer = tf.optimizers.Adam(learning_rate)
 
-    # Create placeholders for inputs, labels
-    _inputs = tf.compat.v1.placeholder(tf.float32,
-                                       shape=[None, time_steps, element_size],
-                                       name='inputs')
-    y = tf.compat.v1.placeholder(tf.float32, shape=[None, num_classes], name='labels')
-
-    # This helper function taken from official TensorFlow documentation,
-    # simply add some ops that take care of logging summaries
-    def variable_summaries(var):
-        with tf.compat.v1.name_scope('summaries'):
-            mean = tf.reduce_mean(input_tensor=var)
-            tf.compat.v1.summary.scalar('mean', mean)
-            with tf.compat.v1.name_scope('stddev'):
-                stddev = tf.sqrt(tf.reduce_mean(input_tensor=tf.square(var - mean)))
-            tf.compat.v1.summary.scalar('stddev', stddev)
-            tf.compat.v1.summary.scalar('max', tf.reduce_max(input_tensor=var))
-            tf.compat.v1.summary.scalar('min', tf.reduce_min(input_tensor=var))
-            tf.compat.v1.summary.histogram('histogram', var)
-
-    # Weights and bias for input and hidden layer
-    with tf.compat.v1.name_scope('rnn_weights'):
-        with tf.compat.v1.name_scope("W_x"):
-            Wx = tf.Variable(tf.zeros([element_size, hidden_layer_size]))
-            variable_summaries(Wx)
-        with tf.compat.v1.name_scope("W_h"):
-            Wh = tf.Variable(tf.zeros([hidden_layer_size, hidden_layer_size]))
-            variable_summaries(Wh)
-        with tf.compat.v1.name_scope("Bias"):
-            b_rnn = tf.Variable(tf.zeros([hidden_layer_size]))
-            variable_summaries(b_rnn)
-
-    def rnn_step(previous_hidden_state, x):
-        current_hidden_state = tf.tanh(
-            tf.matmul(previous_hidden_state, Wh) +
-            tf.matmul(x, Wx) + b_rnn)
-
-        return current_hidden_state
-
-    # Processing inputs to work with scan function
-    # Current input shape: (batch_size, time_steps, element_size)
-    processed_input = tf.transpose(a=_inputs, perm=[1, 0, 2])
-    # Current input shape now: (time_steps,batch_size, element_size)
-
-    initial_hidden = tf.zeros([batch_size, hidden_layer_size])
-    # Getting all state vectors across time
-    all_hidden_states = tf.scan(rnn_step,
-                                processed_input,
-                                initializer=initial_hidden,
-                                name='states')
-
-    # Weights for output layers
-    with tf.compat.v1.name_scope('linear_layer_weights') as scope:
-        with tf.compat.v1.name_scope("W_linear"):
-            Wl = tf.Variable(tf.random.truncated_normal([hidden_layer_size, num_classes],
-                                                        mean=0, stddev=.01))
-            variable_summaries(Wl)
-        with tf.compat.v1.name_scope("Bias_linear"):
-            bl = tf.Variable(tf.random.truncated_normal([num_classes],
-                                                        mean=0, stddev=.01))
-            variable_summaries(bl)
-
-    # Apply linear layer to state vector
-    def get_linear_layer(hidden_state):
-        return tf.matmul(hidden_state, Wl) + bl
-
-    with tf.compat.v1.name_scope('linear_layer_weights') as scope:
-        # Iterate across time, apply linear layer to all RNN outputs
-        all_outputs = tf.map_fn(get_linear_layer, all_hidden_states)
-        # Get Last output -- h_28
-        output = all_outputs[-1]
-        tf.compat.v1.summary.histogram('outputs', output)
-
-    with tf.compat.v1.name_scope('cross_entropy'):
-        cross_entropy = tf.reduce_mean(
-            input_tensor=tf.nn.softmax_cross_entropy_with_logits(logits=output, labels=tf.stop_gradient(y)))
-        tf.compat.v1.summary.scalar('cross_entropy', cross_entropy)
-
-    with tf.compat.v1.name_scope('train'):
-        # Using RMSPropOptimizer
-        train_step = tf.compat.v1.train.RMSPropOptimizer(0.001, 0.9).minimize(cross_entropy)
-
-    with tf.compat.v1.name_scope('accuracy'):
-        correct_prediction = tf.equal(tf.argmax(input=y, axis=1), tf.argmax(input=output, axis=1))
-
-        accuracy = (tf.reduce_mean(input_tensor=tf.cast(correct_prediction, tf.float32))) * 100
-        tf.compat.v1.summary.scalar('accuracy', accuracy)
-
-    # Merge all the summaries
-    merged = tf.compat.v1.summary.merge_all()
-
-    # Get a small test set
-    test_data = mnist.test.images[:batch_size].reshape((-1, time_steps, element_size))
-    test_label = mnist.test.labels[:batch_size]
-
-    with tf.compat.v1.Session() as sess:
-        # Write summaries to LOG_DIR -- used by TensorBoard
-        train_writer = tf.compat.v1.summary.FileWriter(LOG_DIR + '/train',
-                                                       graph=tf.compat.v1.get_default_graph())
-        test_writer = tf.compat.v1.summary.FileWriter(LOG_DIR + '/test',
-                                                      graph=tf.compat.v1.get_default_graph())
-
-        sess.run(tf.compat.v1.global_variables_initializer())
-
-        for i in range(10000):
-
-            batch_x, batch_y = mnist.train.next_batch(batch_size)
-            # Reshape data to get 28 sequences of 28 pixels
-            batch_x = batch_x.reshape((batch_size, time_steps, element_size))
-            summary, _ = sess.run([merged, train_step],
-                                  feed_dict={_inputs: batch_x, y: batch_y})
-            # Add to summaries
-            train_writer.add_summary(summary, i)
-
-            if i % 1000 == 0:
-                acc, loss, = sess.run([accuracy, cross_entropy],
-                                      feed_dict={_inputs: batch_x,
-                                                 y: batch_y})
-                print("Iter " + str(i) + ", Minibatch Loss= " +
-                      "{:.6f}".format(loss) + ", Training Accuracy= " +
-                      "{:.5f}".format(acc))
-            if i % 100 == 0:
-                # Calculate accuracy for 128 mnist test images and
-                # add to summaries
-                summary, acc = sess.run([merged, accuracy],
-                                        feed_dict={_inputs: test_data,
-                                                   y: test_label})
-                test_writer.add_summary(summary, i)
-
-        test_acc = sess.run(accuracy, feed_dict={_inputs: test_data,
-                                                 y: test_label})
-        print("Test Accuracy:", test_acc)
+    for step, (batch_x, batch_y) in enumerate(train_data.take(training_steps), 1):
+        run_optimization(batch_x, batch_y, lstm_net=lstm_net, optimizer=optimizer)
+        if step % display_step == 0:
+            pred = lstm_net(batch_x, is_training=True)
+            loss = cross_entropy_loss(pred, batch_y)
+            acc = accuracy(pred, batch_y)
+            print("step: %i, loss: %f, accuracy: %f" % (step, loss, acc))
 
 
 if __name__ == "__main__":
