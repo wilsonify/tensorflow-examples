faa2b1d08c9c922ad30d4affe5215ca0ca379c68
wilsonify <tom.andrew.wilson@gmail.com>
2019-11-24 17:04:17 -0500

    log %(funcName)s

--- src/tensorflow-examples/tensorflow_examples/config.py
+++ src/tensorflow-examples/tensorflow_examples/config.py
@@ -1,12 +1,17 @@
 import logging
+import os
 
 LOGGING_CONFIG_DICT = dict(
     version=1,
     formatters={
         "simple": {
-            "format": """%(asctime)s | %(name)-12s | %(levelname)-8s | %(message)s"""
+            "format": """%(asctime)s | %(name)s | %(funcName)s | %(levelname)s | %(message)s"""
         }
     },
     handlers={"console": {"class": "logging.StreamHandler", "formatter": "simple"}},
     root={"handlers": ["console"], "level": logging.DEBUG},
 )
+
+HOME_DIR = os.path.expanduser("~")
+
+DATA_DIR = os.path.join(HOME_DIR, "tensorflow-example-data")
