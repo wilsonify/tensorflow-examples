854a24b86946c34fadb356f6445fbc7e33c95b79
Tom Wilson <tom.andrew.wilson@gmail.com>
2019-12-01 11:50:03 -0500

    refactor for main()

--- src/tensorflow-examples/tensorflow_examples/converted_notebooks/dcgan.py
+++ src/tensorflow-examples/tensorflow_examples/converted_notebooks/dcgan.py
@@ -37,40 +37,20 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from IPython import display
 
-# ### Load and prepare the dataset
-# You will use the MNIST dataset to train the generator and the discriminator.
-# The generator will generate handwritten digits resembling the MNIST data.
-
-# In[8]:
-
-
-(TRAIN_IMAGES, TRAIN_LABELS), (_, _) = tf.keras.datasets.mnist.load_data()
-
-# In[9]:
-
-
-TRAIN_IMAGES = TRAIN_IMAGES.reshape(TRAIN_IMAGES.shape[0], 28, 28, 1).astype("float32")
-TRAIN_IMAGES = (TRAIN_IMAGES - 127.5) / 127.5  # Normalize the images to [-1, 1]
-
-# In[10]:
-
-
-BUFFER_SIZE = 60000
-BATCH_SIZE = 256
-
-# In[11]:
-
-
-# Batch and shuffle the data
-TRAIN_DATASET = tf.data.Dataset.from_tensor_slices(TRAIN_IMAGES) \
-    .shuffle(BUFFER_SIZE) \
-    .batch(BATCH_SIZE)
-
-
-# In[12]:
 # Create the models
 # Both the generator and discriminator are defined using the
 # [Keras Sequential API](https://www.tensorflow.org/guide/keras#sequential_model).
+
+BUFFER_SIZE = 60000
+BATCH_SIZE = 256
+# ## Define the training loop
+EPOCHS = 50
+NOISE_DIM = 100
+NUM_EXAMPLES_TO_GENERATE = 16
+
+# This method returns a helper function to compute cross entropy loss
+CROSS_ENTROPY = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+SEED = tf.random.normal([NUM_EXAMPLES_TO_GENERATE, NOISE_DIM])
 
 
 def make_generator_model():
@@ -122,20 +102,6 @@ def make_generator_model():
 
 # Use the (as yet untrained) generator to create an image.
 
-# In[13]:
-
-
-GENERATOR = make_generator_model()
-
-NOISE = tf.random.normal([1, 100])
-GENERATED_IMAGE = GENERATOR(NOISE, training=False)
-
-plt.imshow(GENERATED_IMAGE[0, :, :, 0], cmap="gray")
-
-
-# In[14]:
-
-
 def make_discriminator_model():
     """
     The Discriminator
@@ -165,26 +131,6 @@ def make_discriminator_model():
 # Use the (as yet untrained) discriminator to classify the generated images as real or fake.
 # The model will be trained to output positive values for real images, and negative values for fake images.
 
-# In[15]:
-
-
-DISCRIMINATOR = make_discriminator_model()
-DECISION = DISCRIMINATOR(GENERATED_IMAGE)
-print(DECISION)
-
-# ## Define the loss and optimizers
-# Define loss functions and optimizers for both models.
-
-# In[16]:
-
-
-# This method returns a helper function to compute cross entropy loss
-CROSS_ENTROPY = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-
-
-# In[17]:
-
-
 def discriminator_loss(real_output, fake_output):
     """
     Discriminator loss
@@ -202,9 +148,6 @@ def discriminator_loss(real_output, fake_output):
     return total_loss
 
 
-# In[18]:
-
-
 def generator_loss(fake_output):
     """
     ### Generator loss
@@ -216,120 +159,11 @@ def generator_loss(fake_output):
     :param fake_output:
     :return:
     """
+
     return CROSS_ENTROPY(tf.ones_like(fake_output), fake_output)
 
 
 # The discriminator and the generator optimizers are different since we will train two networks separately.
-
-# In[19]:
-
-
-GENERATOR_OPTIMIZER = tf.keras.optimizers.Adam(1e-4)
-DISCRIMINATOR_OPTIMIZER = tf.keras.optimizers.Adam(1e-4)
-
-# ### Save checkpoints
-# This notebook also demonstrates how to save and restore models,
-# which can be helpful in case a long running training task is interrupted.
-
-# In[20]:
-
-
-CHECKPOINT_DIR = "./training_checkpoints"
-CHECKPOINT_PREFIX = os.path.join(CHECKPOINT_DIR, "ckpt")
-CHECKPOINT = tf.train.Checkpoint(
-    generator_optimizer=GENERATOR_OPTIMIZER,
-    discriminator_optimizer=DISCRIMINATOR_OPTIMIZER,
-    generator=GENERATOR,
-    discriminator=DISCRIMINATOR,
-)
-
-# ## Define the training loop
-# In[21]:
-
-
-EPOCHS = 50
-NOISE_DIM = 100
-NUM_EXAMPLES_TO_GENERATE = 16
-
-# We will reuse this seed overtime (so it's easier)
-# to visualize progress in the animated GIF)
-SEED = tf.random.normal([NUM_EXAMPLES_TO_GENERATE, NOISE_DIM])
-
-
-# In[22]:
-
-
-@tf.function
-def train_step(images):
-    """
-    Notice the use of `tf.function`
-    This annotation causes the function to be "compiled".
-    The training loop begins with generator receiving a random seed as input.
-    That seed is used to produce an image.
-    The discriminator is then used to classify real images
-    (drawn from the training set) and fakes images (produced by the generator).
-    The loss is calculated for each of these models,
-    and the gradients are used to update the generator and discriminator.
-
-    :param images:
-    :return:
-    """
-    noise_rand = tf.random.normal([BATCH_SIZE, NOISE_DIM])
-
-    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-        generated_images = GENERATOR(noise_rand, training=True)
-
-        real_output = DISCRIMINATOR(images, training=True)
-        fake_output = DISCRIMINATOR(generated_images, training=True)
-
-        gen_loss = generator_loss(fake_output)
-        disc_loss = discriminator_loss(real_output, fake_output)
-
-    gradients_of_generator = gen_tape.gradient(gen_loss, GENERATOR.trainable_variables)
-    gradients_of_discriminator = disc_tape.gradient(
-        disc_loss, DISCRIMINATOR.trainable_variables
-    )
-
-    GENERATOR_OPTIMIZER.apply_gradients(
-        zip(gradients_of_generator, GENERATOR.trainable_variables)
-    )
-    DISCRIMINATOR_OPTIMIZER.apply_gradients(
-        zip(gradients_of_discriminator, DISCRIMINATOR.trainable_variables)
-    )
-
-
-# In[23]:
-
-
-def train(dataset, epochs):
-    """
-    handle epoch logic
-    :param dataset:
-    :param epochs:
-    :return:
-    """
-    for epoch in range(epochs):
-        start = time.time()
-
-        for image_batch in dataset:
-            train_step(image_batch)
-
-        # Produce images for the GIF as we go
-        display.clear_output(wait=True)
-        generate_and_save_images(GENERATOR, epoch + 1, SEED)
-
-        # Save the model every 15 epochs
-        if (epoch + 1) % 15 == 0:
-            CHECKPOINT.save(file_prefix=CHECKPOINT_PREFIX)
-
-        print("Time for epoch {} is {} sec".format(epoch + 1, time.time() - start))
-
-    # Generate after the final epoch
-    display.clear_output(wait=True)
-    generate_and_save_images(GENERATOR, epochs, SEED)
-
-
-# In[24]:
 
 
 def generate_and_save_images(model, epoch, test_input):
@@ -357,34 +191,6 @@ def generate_and_save_images(model, epoch, test_input):
     plt.show()
 
 
-# ## Train the model
-# Call the `train()` method defined above to train the generator and discriminator simultaneously.
-# Note, training GANs can be tricky. It's important that the generator and discriminator do not overpower each other
-# (e.g., that they train at a similar rate).
-# At the beginning of the training,
-# the generated images look like random noise.
-# As training progresses, the generated digits will look increasingly real.
-# After about 50 epochs, they resemble MNIST digits.
-# This may take about one minute / epoch with the default settings on Colab.
-
-# In[25]:
-
-
-train(TRAIN_DATASET, EPOCHS)
-
-# Restore the latest checkpoint.
-
-# In[26]:
-
-
-CHECKPOINT.restore(tf.train.latest_checkpoint(CHECKPOINT_DIR))
-
-
-# ## Create a GIF
-
-# In[27]:
-
-
 def display_image(epoch_no):
     """
     # Display a single image using the epoch number
@@ -394,38 +200,168 @@ def display_image(epoch_no):
     return PIL.Image.open("image_at_epoch_{:04d}.png".format(epoch_no))
 
 
-# In[28]:
+def main():
+    """
+    # ### Load and prepare the dataset
+    # You will use the MNIST dataset to train the generator and the discriminator.
+    # The generator will generate handwritten digits resembling the MNIST data.
+
+    :return:
+    """
+
+    (train_images, _), (_, _) = tf.keras.datasets.mnist.load_data()
+
+    train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype("float32")
+    train_images = (train_images - 127.5) / 127.5  # Normalize the images to [-1, 1]
+
+    # Batch and shuffle the data
+    train_dataset = tf.data.Dataset.from_tensor_slices(train_images) \
+        .shuffle(BUFFER_SIZE) \
+        .batch(BATCH_SIZE)
+
+    generator = make_generator_model()
+
+    noise = tf.random.normal([1, 100])
+    generated_image = generator(noise, training=False)
+
+    plt.imshow(generated_image[0, :, :, 0], cmap="gray")
+
+    discriminator = make_discriminator_model()
+    decision = discriminator(generated_image)
+    print(decision)
+
+    # ## Define the loss and optimizers
+    # Define loss functions and optimizers for both models.
+
+    generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+    discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+
+    # ### Save checkpoints
+    # This notebook also demonstrates how to save and restore models,
+    # which can be helpful in case a long running training task is interrupted.
+
+    checkpoint_dir = "./training_checkpoints"
+    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+    checkpoint = tf.train.Checkpoint(
+        generator_optimizer=generator_optimizer,
+        discriminator_optimizer=discriminator_optimizer,
+        generator=generator,
+        discriminator=discriminator,
+    )
+
+    # ## Train the model
+    # Call the `train()` method defined above to train the generator and discriminator simultaneously.
+    # Note, training GANs can be tricky. It's important that the generator and discriminator do not overpower each other
+    # (e.g., that they train at a similar rate).
+    # At the beginning of the training,
+    # the generated images look like random noise.
+    # As training progresses, the generated digits will look increasingly real.
+    # After about 50 epochs, they resemble MNIST digits.
+    # This may take about one minute / epoch with the default settings on Colab.
+
+    @tf.function
+    def train_step(images):
+        """
+        Notice the use of `tf.function`
+        This annotation causes the function to be "compiled".
+        The training loop begins with generator receiving a random seed as input.
+        That seed is used to produce an image.
+        The discriminator is then used to classify real images
+        (drawn from the training set) and fakes images (produced by the generator).
+        The loss is calculated for each of these models,
+        and the gradients are used to update the generator and discriminator.
+
+        :param images:
+        :return:
+        """
+        noise_rand = tf.random.normal([BATCH_SIZE, NOISE_DIM])
+
+        with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+            generated_images = generator(noise_rand, training=True)
+
+            real_output = discriminator(images, training=True)
+            fake_output = discriminator(generated_images, training=True)
+
+            gen_loss = generator_loss(fake_output)
+            disc_loss = discriminator_loss(real_output, fake_output)
+
+        gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+        gradients_of_discriminator = disc_tape.gradient(
+            disc_loss, discriminator.trainable_variables
+        )
+
+        generator_optimizer.apply_gradients(
+            zip(gradients_of_generator, generator.trainable_variables)
+        )
+        discriminator_optimizer.apply_gradients(
+            zip(gradients_of_discriminator, discriminator.trainable_variables)
+        )
+
+    def train(dataset, epochs):
+        """
+        handle epoch logic
+        :param dataset:
+        :param epochs:
+        :return:
+        """
+        for epoch in range(epochs):
+            start = time.time()
+
+            for image_batch in dataset:
+                train_step(image_batch)
+
+            # Produce images for the GIF as we go
+            display.clear_output(wait=True)
+            generate_and_save_images(generator, epoch + 1, SEED)
+
+            # Save the model every 15 epochs
+            if (epoch + 1) % 15 == 0:
+                checkpoint.save(file_prefix=checkpoint_prefix)
+
+            print("Time for epoch {} is {} sec".format(epoch + 1, time.time() - start))
+
+        # Generate after the final epoch
+        display.clear_output(wait=True)
+        generate_and_save_images(generator, epochs, SEED)
+
+    train(train_dataset, EPOCHS)
+
+    # Restore the latest checkpoint.
+
+    checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
+
+    # ## Create a GIF
+
+    display_image(EPOCHS)
+
+    # Use `imageio` to create an animated gif using the images saved during training.
+
+    anim_file = "dcgan.gif"
+
+    with imageio.get_writer(anim_file, mode="I") as writer:
+        filenames = glob.glob("image*.png")
+        filenames = sorted(filenames)
+        last = -1
+        for i, filename in enumerate(filenames):
+            frame = 2 * (i ** 0.5)
+            if round(frame) > round(last):
+                last = frame
+            else:
+                continue
+            image = imageio.imread(filename)
+            writer.append_data(image)
+
+    display.Image(filename=anim_file)
+
+    # ## Next steps
+
+    # This tutorial has shown the complete code necessary to write and train a GAN.
+    # As a next step, you might like to experiment with a different dataset,
+    # for example the Large-scale Celeb Faces Attributes (CelebA) dataset
+    # [available on Kaggle](https://www.kaggle.com/jessicali9530/celeba-dataset/home).
+    # To learn more about GANs we recommend the [NIPS 2016 Tutorial:
+    # Generative Adversarial Networks](https://arxiv.org/abs/1701.00160).
 
 
-display_image(EPOCHS)
-
-# Use `imageio` to create an animated gif using the images saved during training.
-
-# In[29]:
-
-
-ANIM_FILE = "dcgan.gif"
-
-with imageio.get_writer(ANIM_FILE, mode="I") as writer:
-    FILENAMES = glob.glob("image*.png")
-    FILENAMES = sorted(FILENAMES)
-    LAST = -1
-    for i, filename in enumerate(FILENAMES):
-        frame = 2 * (i ** 0.5)
-        if round(frame) > round(LAST):
-            LAST = frame
-        else:
-            continue
-        image = imageio.imread(filename)
-        writer.append_data(image)
-
-display.Image(filename=ANIM_FILE)
-
-# ## Next steps
-
-# This tutorial has shown the complete code necessary to write and train a GAN.
-# As a next step, you might like to experiment with a different dataset,
-# for example the Large-scale Celeb Faces Attributes (CelebA) dataset
-# [available on Kaggle](https://www.kaggle.com/jessicali9530/celeba-dataset/home).
-# To learn more about GANs we recommend the [NIPS 2016 Tutorial:
-# Generative Adversarial Networks](https://arxiv.org/abs/1701.00160).
+if __name__ == "__main__":
+    main()
