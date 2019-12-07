bbbf0dc4ec364c2516f208c35a64acb081c59e9f
wilsonify <tom.andrew.wilson@gmail.com>
2019-12-07 04:28:22 -0500

    black formatting

--- src/tensorflow-examples/tensorflow_examples/converted_notebooks/dcgan.py
+++ src/tensorflow-examples/tensorflow_examples/converted_notebooks/dcgan.py
@@ -46,17 +46,17 @@
 
 # ## What are GANs?
 # [Generative Adversarial Networks](https://arxiv.org/abs/1406.2661) (GANs) are one of the most interesting ideas in computer science today. Two models are trained simultaneously by an adversarial process. A *generator* ("the artist") learns to create images that look real, while a *discriminator* ("the art critic") learns to tell real images apart from fakes.
-# 
+#
 # ![A diagram of a generator and discriminator](./images/gan1.png)
-# 
+#
 # During training, the *generator* progressively becomes better at creating images that look real, while the *discriminator* becomes better at telling them apart. The process reaches equilibrium when the *discriminator* can no longer distinguish real images from fakes.
-# 
+#
 # ![A second diagram of a generator and discriminator](./images/gan2.png)
-# 
+#
 # This notebook demonstrates this process on the MNIST dataset. The following animation shows a series of images produced by the *generator* as it was trained for 50 epochs. The images begin as random noise, and increasingly resemble hand written digits over time.
-# 
+#
 # ![sample output](https://tensorflow.org/images/gan/dcgan.gif)
-# 
+#
 # To learn more about GANs, we recommend MIT's [Intro to Deep Learning](http://introtodeeplearning.com/) course.
 
 # ### Import TensorFlow and other libraries
@@ -71,7 +71,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 try:
     # %tensorflow_version only exists in Colab.
-    get_ipython().run_line_magic('tensorflow_version', '2.x')
+    get_ipython().run_line_magic("tensorflow_version", "2.x")
 except Exception:
     pass
 
@@ -89,7 +89,7 @@ tf.__version__
 
 
 # To generate GIFs
-get_ipython().system('pip install -q imageio')
+get_ipython().system("pip install -q imageio")
 
 # In[7]:
 
@@ -105,7 +105,7 @@ import time
 from IPython import display
 
 # ### Load and prepare the dataset
-# 
+#
 # You will use the MNIST dataset to train the generator and the discriminator. The generator will generate handwritten digits resembling the MNIST data.
 
 # In[8]:
@@ -116,7 +116,7 @@ from IPython import display
 # In[9]:
 
 
-train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype("float32")
 train_images = (train_images - 127.5) / 127.5  # Normalize the images to [-1, 1]
 
 # In[10]:
@@ -129,15 +129,19 @@ BATCH_SIZE = 256
 
 
 # Batch and shuffle the data
-train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+train_dataset = (
+    tf.data.Dataset.from_tensor_slices(train_images)
+        .shuffle(BUFFER_SIZE)
+        .batch(BATCH_SIZE)
+)
 
 
 # ## Create the models
-# 
+#
 # Both the generator and discriminator are defined using the [Keras Sequential API](https://www.tensorflow.org/guide/keras#sequential_model).
 
 # ### The Generator
-# 
+#
 # The generator uses `tf.keras.layers.Conv2DTranspose` (upsampling) layers to produce an image from a seed (random noise). Start with a `Dense` layer that takes this seed as input, then upsample several times until you reach the desired image size of 28x28x1. Notice the `tf.keras.layers.LeakyReLU` activation for each layer, except the output layer which uses tanh.
 
 # In[12]:
@@ -152,17 +156,29 @@ def make_generator_model():
     model.add(layers.Reshape((7, 7, 256)))
     assert model.output_shape == (None, 7, 7, 256)  # Note: None is the batch size
 
-    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    model.add(
+        layers.Conv2DTranspose(
+            128, (5, 5), strides=(1, 1), padding="same", use_bias=False
+        )
+    )
     assert model.output_shape == (None, 7, 7, 128)
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
-    model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    model.add(
+        layers.Conv2DTranspose(
+            64, (5, 5), strides=(2, 2), padding="same", use_bias=False
+        )
+    )
     assert model.output_shape == (None, 14, 14, 64)
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
-    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+    model.add(
+        layers.Conv2DTranspose(
+            1, (5, 5), strides=(2, 2), padding="same", use_bias=False, activation="tanh"
+        )
+    )
     assert model.output_shape == (None, 28, 28, 1)
 
     return model
@@ -178,11 +194,11 @@ generator = make_generator_model()
 noise = tf.random.normal([1, 100])
 generated_image = generator(noise, training=False)
 
-plt.imshow(generated_image[0, :, :, 0], cmap='gray')
+plt.imshow(generated_image[0, :, :, 0], cmap="gray")
 
 
 # ### The Discriminator
-# 
+#
 # The discriminator is a CNN-based image classifier.
 
 # In[14]:
@@ -190,12 +206,15 @@ plt.imshow(generated_image[0, :, :, 0], cmap='gray')
 
 def make_discriminator_model():
     model = tf.keras.Sequential()
-    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
-                            input_shape=[28, 28, 1]))
+    model.add(
+        layers.Conv2D(
+            64, (5, 5), strides=(2, 2), padding="same", input_shape=[28, 28, 1]
+        )
+    )
     model.add(layers.LeakyReLU())
     model.add(layers.Dropout(0.3))
 
-    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding="same"))
     model.add(layers.LeakyReLU())
     model.add(layers.Dropout(0.3))
 
@@ -215,9 +234,9 @@ decision = discriminator(generated_image)
 print(decision)
 
 # ## Define the loss and optimizers
-# 
+#
 # Define loss functions and optimizers for both models.
-# 
+#
 
 # In[16]:
 
@@ -227,7 +246,7 @@ cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
 
 # ### Discriminator loss
-# 
+#
 # This method quantifies how well the discriminator is able to distinguish real images from fakes. It compares the discriminator's predictions on real images to an array of 1s, and the discriminator's predictions on fake (generated) images to an array of 0s.
 
 # In[17]:
@@ -264,16 +283,18 @@ discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
 # In[20]:
 
 
-checkpoint_dir = './training_checkpoints'
+checkpoint_dir = "./training_checkpoints"
 checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
-checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
-                                 discriminator_optimizer=discriminator_optimizer,
-                                 generator=generator,
-                                 discriminator=discriminator)
+checkpoint = tf.train.Checkpoint(
+    generator_optimizer=generator_optimizer,
+    discriminator_optimizer=discriminator_optimizer,
+    generator=generator,
+    discriminator=discriminator,
+)
 
 # ## Define the training loop
-# 
-# 
+#
+#
 
 # In[21]:
 
@@ -308,10 +329,16 @@ def train_step(images):
         disc_loss = discriminator_loss(real_output, fake_output)
 
     gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
-    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(
+        disc_loss, discriminator.trainable_variables
+    )
 
-    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
-    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+    generator_optimizer.apply_gradients(
+        zip(gradients_of_generator, generator.trainable_variables)
+    )
+    discriminator_optimizer.apply_gradients(
+        zip(gradients_of_discriminator, discriminator.trainable_variables)
+    )
 
 
 # In[23]:
@@ -326,26 +353,22 @@ def train(dataset, epochs):
 
         # Produce images for the GIF as we go
         display.clear_output(wait=True)
-        generate_and_save_images(generator,
-                                 epoch + 1,
-                                 seed)
+        generate_and_save_images(generator, epoch + 1, seed)
 
         # Save the model every 15 epochs
         if (epoch + 1) % 15 == 0:
             checkpoint.save(file_prefix=checkpoint_prefix)
 
-        print('Time for epoch {} is {} sec'.format(epoch + 1, time.time() - start))
+        print("Time for epoch {} is {} sec".format(epoch + 1, time.time() - start))
 
     # Generate after the final epoch
     display.clear_output(wait=True)
-    generate_and_save_images(generator,
-                             epochs,
-                             seed)
+    generate_and_save_images(generator, epochs, seed)
 
 
 # **Generate and save images**
-# 
-# 
+#
+#
 
 # In[24]:
 
@@ -359,16 +382,16 @@ def generate_and_save_images(model, epoch, test_input):
 
     for i in range(predictions.shape[0]):
         plt.subplot(4, 4, i + 1)
-        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
-        plt.axis('off')
+        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap="gray")
+        plt.axis("off")
 
-    plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
+    plt.savefig("image_at_epoch_{:04d}.png".format(epoch))
     plt.show()
 
 
 # ## Train the model
 # Call the `train()` method defined above to train the generator and discriminator simultaneously. Note, training GANs can be tricky. It's important that the generator and discriminator do not overpower each other (e.g., that they train at a similar rate).
-# 
+#
 # At the beginning of the training, the generated images look like random noise. As training progresses, the generated digits will look increasingly real. After about 50 epochs, they resemble MNIST digits. This may take about one minute / epoch with the default settings on Colab.
 
 # In[25]:
@@ -385,14 +408,14 @@ checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
 
 
 # ## Create a GIF
-# 
+#
 
 # In[27]:
 
 
 # Display a single image using the epoch number
 def display_image(epoch_no):
-    return PIL.Image.open('image_at_epoch_{:04d}.png'.format(epoch_no))
+    return PIL.Image.open("image_at_epoch_{:04d}.png".format(epoch_no))
 
 
 # In[28]:
@@ -405,10 +428,10 @@ display_image(EPOCHS)
 # In[29]:
 
 
-anim_file = 'dcgan.gif'
+anim_file = "dcgan.gif"
 
-with imageio.get_writer(anim_file, mode='I') as writer:
-    filenames = glob.glob('image*.png')
+with imageio.get_writer(anim_file, mode="I") as writer:
+    filenames = glob.glob("image*.png")
     filenames = sorted(filenames)
     last = -1
     for i, filename in enumerate(filenames):
@@ -424,7 +447,7 @@ with imageio.get_writer(anim_file, mode='I') as writer:
 
 import IPython
 
-if IPython.version_info > (6, 2, 0, ''):
+if IPython.version_info > (6, 2, 0, ""):
     display.Image(filename=anim_file)
 
 # If you're working in Colab you can download the animation with the code below:
@@ -440,7 +463,7 @@ else:
     files.download(anim_file)
 
 # ## Next steps
-# 
+#
 
 # This tutorial has shown the complete code necessary to write and train a GAN. As a next step, you might like to experiment with a different dataset, for example the Large-scale Celeb Faces Attributes (CelebA) dataset [available on Kaggle](https://www.kaggle.com/jessicali9530/celeba-dataset/home). To learn more about GANs we recommend the [NIPS 2016 Tutorial: Generative Adversarial Networks](https://arxiv.org/abs/1701.00160).
 #
